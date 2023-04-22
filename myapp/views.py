72a3a35ac282a8aa0c60bb54fa6c719e51a419f0
JAYDEEPMISHR <jaydeepmishra1996@gmail.com>
2023-04-23 00:08:37 +0530

    23 Apr 2023

---
+++ myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import User
+# Create your views here.
+
+def index(request):
+	return render(request,'index.html')
+
+def signup(request):
+	return render(request,'signup.html')
